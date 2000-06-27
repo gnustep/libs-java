9c06ee3b5e1f5c8ddcd8025e57fb1c56a35242b5
nico <nico@72102866-910b-0410-8b05-ffd578937521>
2000-06-27 14:31:00 +0000

    New file
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/libs/java/trunk@6822 72102866-910b-0410-8b05-ffd578937521

---
+++ Source/GSJNI.h
@@ -0,0 +1,35 @@
+/* GSJNI.h - Useful utilities for JNI programming with GNUstep
+   Copyright (C) 2000 Free Software Foundation, Inc.
+   
+   Written by:  Nicola Pero <nicola@brainstorm.co.uk>
+   Date: June 2000
+   
+   This file is part of JIGS, the GNUstep Java Interface Library.
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+   
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+   
+   You should have received a copy of the GNU Library General Public
+   License along with this library; if not, write to the Free
+   Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111 USA.
+   */ 
+
+#ifndef __GSJNI_h_GNUSTEP_JAVA_INCLUDE
+#define __GSJNI_h_GNUSTEP_JAVA_INCLUDE
+
+
+#include <jni.h>
+#include <Foundation/Foundation.h>
+#include "GSJNICache.h"
+#include "GSJNIClass.h"
+#include "GSJNIDebug.h"
+#include "GSJNIString.h"
+
+#endif /*__GSJNI_h_GNUSTEP_JAVA_INCLUDE*/
