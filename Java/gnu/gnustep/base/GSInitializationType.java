91b8bfde5b60e090d76d63f7897147559a5a966f
Nicola Pero <n.pero@mi.flashnet.it>
2000-07-26 10:54:00 +0000

    New file
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/libs/java/trunk@6977 72102866-910b-0410-8b05-ffd578937521

---
+++ Java/gnu/gnustep/base/GSInitializationType.java
@@ -0,0 +1,38 @@
+/* GSInitializationType.java - Class describing the type of init to perform
+   Copyright (C) 2000 Free Software Foundation, Inc.
+
+   Written by:  Nicola Pero <nicola@brainstorm.co.uk>
+   Date: July 2000
+   
+   This file is part of the GNUstep Java Interface Library.
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+   
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+   
+   You should have received a copy of the GNU Library General Public
+   License along with this library; if not, write to the Free
+   Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111 USA.  */
+package gnu.gnustep.base;
+
+/*
+ * There are major differences between Java constructor system 
+ * and Objective-C init/alloc system.  
+ * I didn't want to write this class, but it seems the only easy 
+ * way to make things clean.  See NSObject.java for more info.
+ *
+ */
+
+public class GSInitializationType 
+{
+  public GSInitializationType ()
+  {
+    super ();
+  }
+}
