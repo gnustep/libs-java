39d367921dcdc4d5e7f15a28bec4ca49a2d2170f
Nicola Pero <n.pero@mi.flashnet.it>
2000-07-29 09:49:57 +0000

    New file
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/libs/java/trunk@7024 72102866-910b-0410-8b05-ffd578937521

---
+++ Source/JIGSMapper.h
@@ -0,0 +1,190 @@
+/* JIGSMapper.h - Java proxies of Objc objects
+   Copyright (C) 2000 Free Software Foundation, Inc.
+   
+   Written by:  Nicola Pero <nicola@brainstorm.co.uk>
+   Date: July 2000
+   
+   This file is part of the GNUstep Java Interface Library.
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+   
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+   
+   You should have received a copy of the GNU Library General Public
+   License along with this library; if not, write to the Free
+   Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111 USA.
+   */ 
+
+#ifndef __JIGSMapper_h_GNUSTEP_JAVA_INCLUDE
+#define __JIGSMapper_h_GNUSTEP_JAVA_INCLUDE
+
+#include <jni.h>
+#include <Foundation/Foundation.h>
+
+/*
+ * JIGSMapper contains code to manage mapping tables.
+
+ * Each proxy (both on Java and Objc side) contains an ivar pointing 
+ * to its real object 
+
+ * We need a 'mapping table' to make a lookup in the reverse
+ * direction, ie, given a real object, find, if any, the proxy
+ * pointing to it (this is needed so that we don't create more than
+ * one proxy to a single real object).  The mapping table is actually
+ * used only when passing objects as arguments or return values
+ * through the interface. 
+ */
+
+/* Public functions to be used in the java native method implementations
+   to map jobjects to/from ids */
+
+/*
+ * Use the following to get the id of the 'this' argument of a java
+ * native wrapper method call.  This is fast because we know that
+ * 'this' is a gnu/gnustep/base/NSObject.
+ *
+ * *Never* use with an argument different from the 'this' argument of
+ * a native method call: the overhead of the generic method is tiny,
+ * but it's safer.  The following will crash if you pass a generic
+ * argument.  */
+
+inline id JIGSIdFromThis (JNIEnv *env, jobject this);
+
+/*
+ * The following functions are the more general and slow of the proxying 
+ * functions. 
+ *
+ * Given an object, they check its class to understand if it is a
+ * proxy or a real object.  They convert it in both cases, creating a
+ * proxy if needed.  
+ *
+ * Whenever you need to convert a jobject to/from an id, the following 
+ * functions should do the job.
+ *
+ * Exceptions: 
+ * For strings, if you can be sure they are strings, you can use
+ * GSJNI_NSStringFromJString directly.  Arguments of java methods have
+ * their classes checked by the java runtime so if the argument is
+ * declared of type String, using GSJNI_NSStringFromJString should be
+ * safe.  
+ *
+ * To morph a java object of class NSPoint, NSSize, NSRect or NSRange
+ * into a C struct, use the appropriate functions in JIGSBaseStruct.  
+ */
+
+jobject JIGSJobjectFromId (JNIEnv *env, id object);
+
+id JIGSIdFromJobject (JNIEnv *env, jobject object);
+
+/*
+ * Create new proxies. 
+ * 
+ * Whenever you create a new object and need to return an jobject for
+ * it, you may follow the general road, which is call
+ * JIGSJobjectFromId or JIGSIdFromJobject (which will create
+ * automatically a new proxy for you if there is no proxy).
+ * But, if you are sure that the new object has no proxy (for example 
+ * because you just created it by copying another object), you can 
+ * just create the new proxy directly (this is faster).  
+ * Warning: if you are not sure that the object has no proxy, don't
+ * call these methods.  */
+
+id JIGSCreateNewObjcProxy (JNIEnv *env, jobject proxiedJava);
+
+jobject JIGSCreateNewJavaProxy (JNIEnv *env, id proxiedObject);
+
+/*
+ * Register a new java proxy class with the mapper.
+ * For example, if you provide a java class with a native
+ * implementation which wraps the objective-C class 'MyClass', then
+ * you need to register your proxy class with the JIGSMapper so that
+ * it knows that, when it needs to create a proxy for an objective-C
+ * object of class MyClass, it can create a java object of your proxy 
+ * class rather than a java object of the generic proxy class 'NSObject'. 
+ *
+ * This is necessary since we can't rebuild the full java name from 
+ * the short name - eg, @"MyClass" could be gnu.gnustep.base.MyClass 
+ * or gnu.gnustep.gui.MyClass or com.your_company.MyClass or 
+ * gnu.gnustep.yourFramework.MyClass: we can't know - you need to 
+ * register the mapping of classes.
+ *
+ * In your library wrapper, you should call in JNI_OnLoad this
+ * function for each class you have wrapped.
+ */
+
+void JIGSRegisterJavaProxyClass (JNIEnv *env, NSString *fullJavaClassName, 
+				 NSString *objcClassName);
+
+/*
+ * END OF PUBLIC FUNCTIONS
+ * The following functions are private to the JIGS internals.
+ * You should (usually) never need to access them directly.
+ *
+ */
+
+/*
+ * We store pointers to the real object in the java proxy, 
+ * in its realObject ivar, which is of type jlong (64 bit).  
+ * Use the following macros to convert jlong to/from id.
+ *
+ */
+#if GS_SIZEOF_VOIDP == 4 /* 32 bit machine */
+
+#define JIGS_JLONG_TO_ID(p) (((union {jlong java; id objc[2];}) p).objc[0])
+#define JIGS_ID_TO_JLONG(p) ({union {jlong java; id objc[2];} q; q.objc[0] = p; q.objc[1] = 0; q.java;})
+
+#elif GS_SIZEOF_VOIDP == 8 /* 64 bit machine */
+
+#define JIGS_JLONG_TO_ID(p) ((id)(p))
+#define JIGS_ID_TO_JLONG(p) ((jlong)(p))
+
+#else /* a different or undefined GS_SIZEOF_VOIDP */
+
+#ifdef GS_SIZEOF_VOIDP
+#error Only machines with sizeof (void *) == 4 (32 bit) or 8 (64 bit) are supported.  Your machine has a different sizeof (void *) !
+#else /* GS_SIZEOF_VOIDP undefined */
+#error Undefined GS_SIZEOF_VOIDP.  Make sure your gnustep-core library version is at least 0.6.6.
+#endif 
+
+#endif
+
+/*
+ * Initializes the maps and some cache. 
+ * Safe to be called many times, but of course useless.
+ * Called by JIGSInit.
+ *
+ */
+void _JIGSMapperInitialize (JNIEnv *env);
+
+/*
+ * Insert or remove objects from the maps. 
+ */
+
+/*
+ * _JIGSMapperAddJavaProxy is to be used only in NSObject's native
+ * constructors. (and in JIGSMapperCreateNewJavaProxy)
+ */
+inline void _JIGSMapperAddJavaProxy (JNIEnv *env, id objc, jobject java);
+/*
+ * _JIGSMapperRemoveJavaProxy is to be used only in the finalize
+ * method in (java) gnu.gnustep.base.NSObject
+ */
+inline void _JIGSMapperRemoveJavaProxy (JNIEnv *env, id objc);
+/*
+ * _JIGSMapperAddObjcProxy is to be used only in the constructors in
+ * JIGSProxyIMP (and in JIGSMapperCreateNewObjcProxy)
+ */
+inline void _JIGSMapperAddObjcProxy (JNIEnv *env, jobject java, id objc);
+/* 
+ * _JIGSMapperRemoveObjcProxy is to be used only in the -dealloc
+ * method of (objc) java.lang.Object.  
+ */
+inline void _JIGSMapperRemoveObjcProxy (JNIEnv *env, jobject java);
+
+#endif /* __JIGSMapper_h_GNUSTEP_JAVA_INCLUDE */
