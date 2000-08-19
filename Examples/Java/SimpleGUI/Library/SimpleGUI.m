e31a6d65ecc8bba1f1cfe71b0832be1eff2b47f7
Nicola Pero <n.pero@mi.flashnet.it>
2000-08-19 18:42:37 +0000

    Removed method new
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/libs/java/trunk@7228 72102866-910b-0410-8b05-ffd578937521

--- Examples/Java/SimpleGUI/Library/SimpleGUI.m
+++ Examples/Java/SimpleGUI/Library/SimpleGUI.m
@@ -24,11 +24,6 @@
 #include "SimpleGUI.h"
 
 @implementation SimpleGUI : NSObject
-+ (id) new
-{
-  return [[self alloc] init];
-}
-
 - (id) init
 {
   return [self initWithTitle: @"SimpleGUI app"];
