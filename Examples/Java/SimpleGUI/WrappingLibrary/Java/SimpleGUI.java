36cf1b19dfb0ae126fab6bf66e61cba28de2019f
nico <nico@72102866-910b-0410-8b05-ffd578937521>
2001-02-09 12:00:49 +0000

    Updated for new way of wrapping initializers
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/libs/java/trunk@9083 72102866-910b-0410-8b05-ffd578937521

--- Examples/Java/SimpleGUI/WrappingLibrary/Java/SimpleGUI.java
+++ Examples/Java/SimpleGUI/WrappingLibrary/Java/SimpleGUI.java
@@ -54,14 +54,14 @@ class SimpleGUI extends NSObject
   public SimpleGUI (String title)
    {
      super (ALLOC_ONLY);
-     this.initWithTitle (title);
+     realObject = this.initWithTitle (title);
    }
 
   /* Objective-C methods which are exposed - for each of these methods, 
    * a native implementation must be supplied on the objective-C side 
    */
-  /* initXxx: methods are wrapped in the same way as any other method */ 
-  native void initWithTitle (String title);
+  /* initXxx: methods are wrapped in a special way  */ 
+  native long initWithTitle (String title);
   public native void start ();
   public native void addButtonWithTitle (String title, int tag);
   public native void setDelegate (Object aDelegate);
