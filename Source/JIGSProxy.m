96546f2f89013b785efb150f1bf5c7e7d16d02ca
Nicola Pero <n.pero@mi.flashnet.it>
2001-05-06 19:08:33 +0000

    Implemented key/value coding for pure java objects
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/libs/java/trunk@9864 72102866-910b-0410-8b05-ffd578937521

--- Source/JIGSProxy.m
+++ Source/JIGSProxy.m
@@ -29,6 +29,10 @@
 #include "NSJavaVirtualMachine.h"
 #include "JIGSMapper.h"
 
+static jclass JIGSKeyValueCoding = NULL;
+static jmethodID JIGSValueForKey = NULL;
+static jmethodID JIGSTakeValueForKey = NULL;
+
 // This is roughly the implementation of 'dealloc' of our java.lang.Object 
 // class.  When the class is created at run-time, this method is added to it.
 void _java_lang_Object__dealloc_ (id rcv, SEL sel)
@@ -42,7 +46,7 @@ void _java_lang_Object__dealloc_ (id rcv, SEL sel)
   realObject = ((_java_lang_Object *)(rcv))->realObject;
   _JIGSMapperRemoveObjcProxy (env, realObject);
   (*env)->DeleteGlobalRef (env, realObject);
-
+  
   // Following code is the equivalent of [super dealloc]
   if (super_dealloc == 0)
     {
@@ -52,6 +56,120 @@ void _java_lang_Object__dealloc_ (id rcv, SEL sel)
   super_dealloc (rcv, sel);
 }
 
+// This is the implementation of 'handleQueryWithUnboundKey:' of our
+// java.lang.Object class.  When the class is created at run-time,
+// this method is added to it.
+id _java_lang_Object__handleQueryWithUnboundKey_ (id rcv, SEL sel, 
+						  NSString *key)
+{
+  JNIEnv *env = JIGSJNIEnv ();
+  static IMP super_handleQuery = 0;
+
+  /* We basically execute the Java call
+     
+     gnu.gnustep.java.JIGSKeyValueCoding.jvalueForKey (rcv, key);
+     
+     which does it - or throws an exception if a problem is found.
+  */
+  if (JIGSValueForKey == NULL)
+    {
+      JIGSValueForKey = (*env)->GetStaticMethodID 
+	(env, JIGSKeyValueCoding, "jvalueForKey",
+	 "(Ljava/lang/Object;Ljava/lang/String;)Ljava/lang/Object;");
+    }
+  
+  if (JIGSValueForKey != NULL)
+    {
+      jobject realObject;
+      jobject result;
+      
+      realObject = ((_java_lang_Object *)(rcv))->realObject;
+      
+      result = (*env)->CallStaticObjectMethod (env, JIGSKeyValueCoding, 
+					       JIGSValueForKey, 
+					       realObject,
+					       JIGSJstringFromNSString (env, 
+									key));
+      if ((*env)->ExceptionCheck (env))
+	{
+	  (*env)->ExceptionClear (env);
+	}
+      else
+	{
+	  /* No exception - must have worked ! */
+	  return JIGSIdFromJobject (env, result);
+	}
+    }
+
+
+  // Following code is the equivalent of 
+  // return [super handleQueryWithUnboundKey: key]
+  if (super_handleQuery == 0)
+    {
+      super_handleQuery = get_imp (NSClassFromString (@"NSObject"), 
+				   @selector (handleQueryWithUnboundKey:));
+    }
+  return super_handleQuery (rcv, sel, key);
+}
+
+
+// This is the implementation of 'handleTakeValue:forUnboundKey:' of
+// our java.lang.Object class.  When the class is created at run-time,
+// this method is added to it.
+void _java_lang_Object__handleTakeValueForUnboundKey_ (id rcv, SEL sel, 
+						       id value, NSString *key)
+{
+  JNIEnv *env = JIGSJNIEnv ();
+  static IMP super_handleTakeValue = 0;
+
+  /* We basically execute the Java call
+     
+     gnu.gnustep.java.JIGSKeyValueCoding.jtakeValueForKey (rcv, value, key);
+     
+     which does it - or throws an exception if a problem is found.
+  */
+  if (JIGSTakeValueForKey == NULL)
+    {
+      JIGSTakeValueForKey = (*env)->GetStaticMethodID 
+	(env, JIGSKeyValueCoding, "jtakeValueForKey",
+	 "(Ljava/lang/Object;Ljava/lang/Object;Ljava/lang/String;)V");
+    }
+  
+  if (JIGSTakeValueForKey != NULL)
+    {
+      jobject realObject;
+
+      realObject = ((_java_lang_Object *)(rcv))->realObject;
+
+      (*env)->CallStaticVoidMethod (env, JIGSKeyValueCoding, 
+				    JIGSTakeValueForKey, 
+				    realObject,
+				    JIGSJobjectFromId (env, value), 
+				    JIGSJstringFromNSString (env, key));
+
+      if ((*env)->ExceptionCheck (env))
+	{
+	  (*env)->ExceptionClear (env);
+	}
+      else
+	{
+	  /* No exception - must have worked ! */
+	  return;
+	}
+    }
+  
+  // Following code is the equivalent of 
+  // [super handleTakeValue: value  ofUnboundKey: key]
+  if (super_handleTakeValue == 0)
+    {
+      super_handleTakeValue = get_imp 
+	(NSClassFromString (@"NSObject"), 
+	 @selector (handleTakeValue:forUnboundKey:));
+    }
+  super_handleTakeValue (rcv, sel, value, key);
+}
+
+
 /*
  * Register the java.lang.Object class
  */
@@ -59,16 +177,44 @@ void JIGSRegisterJavaRootClass (JNIEnv *env)
 {
   MethodList *ml;
   const char *signature;
+
+  // Save pointer to JIGSKeyValueCoding class
+  JIGSKeyValueCoding = GSJNI_NewClassCache 
+    (env, "gnu/gnustep/java/JIGSKeyValueCoding");
+
+  if (JIGSKeyValueCoding == NULL)
+    {
+      NSLog (@"Error - Could not find gnu.gnustep.java.JIGSKeyValueCoding");
+    }
+  
   
   // Register java.lang.Object class
   _JIGS_register_java_class_simple (env, @"java.lang.Object", 
 				    @"NSObject", YES);
  
-  // Add to it the dealloc method
-  ml = ObjcUtilities_alloc_method_list (1);
+  // Add to it the custom dealloc, handleQueryWithUnboundKey: and
+  // handleTakeValue:forUnboundKey: methods
+  ml = ObjcUtilities_alloc_method_list (3);
+
+  /* dealloc */
   signature = ObjcUtilities_build_runtime_Objc_signature ("v@:");
   ObjcUtilities_insert_method_in_list (ml, 0, "dealloc", signature, 
 				       (IMP)_java_lang_Object__dealloc_);
+
+  /* handleQueryWithUnboundKey: */
+  signature = ObjcUtilities_build_runtime_Objc_signature ("@@:@");
+  ObjcUtilities_insert_method_in_list 
+    (ml, 1, "handleQueryWithUnboundKey:", 
+     signature, 
+     (IMP)_java_lang_Object__handleQueryWithUnboundKey_);
+
+  /* handleTakeValue:forUnboundKey: */
+  signature = ObjcUtilities_build_runtime_Objc_signature ("v@:@@");
+  ObjcUtilities_insert_method_in_list 
+    (ml, 2, "handleTakeValue:forUnboundKey:", 
+     signature, 
+     (IMP)_java_lang_Object__handleTakeValueForUnboundKey_);
+
   ObjcUtilities_register_method_list (NSClassFromString (@"java.lang.Object"),
 				      ml);
 }
