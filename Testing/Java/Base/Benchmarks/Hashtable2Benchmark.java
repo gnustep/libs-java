4b0cb7f0a25f95ac3e0c76af3a0c702def074506
Nicola Pero <n.pero@mi.flashnet.it>
2001-11-25 12:51:32 +0000

    New benchies added
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/libs/java/trunk@11533 72102866-910b-0410-8b05-ffd578937521

---
+++ Testing/Java/Base/Benchmarks/Hashtable2Benchmark.java
@@ -0,0 +1,48 @@
+/* 
+
+   Copyright (C) 2001 Free Software Foundation, Inc.
+
+   Author:  Nicola Pero <n.pero@mi.flashnet.it>
+   Date: November 2001
+   
+   This file is part of GNUstep.
+   
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
+   
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+   
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA. */
+
+import java.util.*;
+
+public class Hashtable2Benchmark implements Benchmark
+{
+  Hashtable a;
+
+  public Hashtable2Benchmark ()
+  {
+    a = new Hashtable ();
+    a.put ("Nicola", "Name");
+    a.put ("Uno", "One");
+    a.put ("Due", "Two");
+    a.put ("Tre", "Three");
+  }
+
+  public String name ()
+  {
+    return "Getting the string value for a string key from a Hashtable";
+  }
+
+  public Object executeBasicOperation ()
+  {
+     return a.get ("Nicola");
+  }
+}
