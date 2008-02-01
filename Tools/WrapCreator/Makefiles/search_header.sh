2d8b30a966944a890885db605ff40209ebd4d6a6
nicola <nicola@72102866-910b-0410-8b05-ffd578937521>
2008-02-01 14:36:15 +0000

    Updated search_header.sh script to work with gnustep-make v2, do proper quoting, and use /bin/sh instead of /bin/bash
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/libs/java/trunk@26017 72102866-910b-0410-8b05-ffd578937521

--- Tools/WrapCreator/Makefiles/search_header.sh
+++ Tools/WrapCreator/Makefiles/search_header.sh
@@ -1,11 +1,11 @@
-#! /bin/bash
+#! /bin/sh
 #
 #  Looks for a header file in ./, then in the GNUSTEP standard header dirs
 #
-#  Copyright (C) 2001 Free Software Foundation, Inc.
+#  Copyright (C) 2001-2008 Free Software Foundation, Inc.
 #
-#  Author:  Nicola Pero <n.pero@mi.flashnet.it>
-#  Date: May 2001
+#  Author:  Nicola Pero <nicola.pero@meta-innovation.com>
+#  Date: May 2001, January 2008
 #   
 #  This file is part of GNUstep.
 #   
@@ -23,23 +23,23 @@
 #  along with this program; if not, write to the Free Software
 #  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA. */
 
-header_file=$1;
-header_file_dir=$2;
+header_file="$1";
+header_file_dir="$2";
 current_dir=`pwd`;
 
 # If a second argument was given, it is a directory to search the
 # header in - try that first
 if [ -n "$header_file_dir" ]; then 
 
-  case $header_file_dir in
+  case "$header_file_dir" in
     /*) # An absolute path
         ;;
      *) # A relative path
-        header_file_dir=$current_dir/$header_file_dir;;
+        header_file_dir="$current_dir/$header_file_dir";;
   esac
 
   if [ -f "$header_file_dir/$header_file" ]; then
-    echo $header_file_dir/$header_file;
+    echo "$header_file_dir/$header_file";
     exit 0;
   fi
 
@@ -47,38 +47,60 @@ fi
 
 
 
-attempt=$current_dir/$header_file;
+attempt="$current_dir/$header_file";
 
-if [ -f $attempt ]; then 
-  echo $attempt;
+if [ -f "$attempt" ]; then 
+  echo "$attempt";
   exit 0;
 fi
 
-attempt=$GNUSTEP_USER_ROOT/Headers/$header_file;
+# For gnustep-make v2, we need to get the values of all the
+# GNUSTEP_XXX_HEADERS variables.  This seems to be the most
+# backwards-compatible way of doing it.  In gnustep-make v1, it will
+# just source GNUstep.sh again with no changes.
+GNUSTEP_SH_EXPORT_ALL_VARIABLES=yes
+. $GNUSTEP_MAKEFILES/GNUstep.sh
+unset GNUSTEP_SH_EXPORT_ALL_VARIABLES
 
-if [ -f $attempt ]; then 
-  echo $attempt;
+# If GNUSTEP_USER_HEADERS is undefined, we may be running under
+# gnustep-make v1; try using GNUSTEP_USER_ROOT then.
+if [ -z "$GNUSTEP_USER_HEADERS" ]; then
+  GNUSTEP_USER_HEADERS="$GNUSTEP_USER_ROOT/Headers"
+fi
+attempt="$GNUSTEP_USER_HEADERS/$header_file"
+
+if [ -f "$attempt" ]; then 
+  echo "$attempt";
   exit 0;
 fi
 
-attempt=$GNUSTEP_LOCAL_ROOT/Headers/$header_file;
+if [ -z "$GNUSTEP_LOCAL_HEADERS" ]; then
+  GNUSTEP_LOCAL_HEADERS="$GNUSTEP_LOCAL_ROOT/Headers"
+fi
+attempt="$GNUSTEP_LOCAL_HEADERS/$header_file"
 
-if [ -f $attempt ]; then 
-  echo $attempt;
+if [ -f "$attempt" ]; then 
+  echo "$attempt";
   exit 0;
 fi
 
-attempt=$GNUSTEP_NETWORK_ROOT/Headers/$header_file;
+if [ -z "$GNUSTEP_NETWORK_HEADERS" ]; then
+  GNUSTEP_NETWORK_HEADERS="$GNUSTEP_NETWORK_ROOT/Headers"
+fi
+attempt="$GNUSTEP_NETWORK_HEADERS/$header_file"
 
-if [ -f $attempt ]; then 
-  echo $attempt;
+if [ -f "$attempt" ]; then 
+  echo "$attempt";
   exit 0;
 fi
 
-attempt=$GNUSTEP_SYSTEM_ROOT/Headers/$header_file;
+if [ -z "$GNUSTEP_SYSTEM_HEADERS" ]; then
+  GNUSTEP_SYSTEM_HEADERS="$GNUSTEP_SYSTEM_ROOT/Headers"
+fi
+attempt="$GNUSTEP_SYSTEM_HEADERS/$header_file"
 
-if [ -f $attempt ]; then 
-  echo $attempt;
+if [ -f "$attempt" ]; then 
+  echo "$attempt";
   exit 0;
 fi
 
