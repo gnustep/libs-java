face1ab6cfaeaafdc15bbe0b5c7203f206208909
rfm <rfm@72102866-910b-0410-8b05-ffd578937521>
2009-03-19 11:03:09 +0000

    Add neew types
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/libs/java/trunk@28097 72102866-910b-0410-8b05-ffd578937521

---
+++ Tools/WrapCreator/WCCGFloatType.m
@@ -0,0 +1,56 @@
+/* WCCGFloatType.m                                               -*-objc-*-
+
+   Copyright (C) 2000 Free Software Foundation, Inc.
+
+   Author:  Nicola Pero <nicola@brainstorm.co.uk>
+   Date: September 2000
+   
+   This file is part of JIGS, the GNUstep Java Interface.
+   
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
+   
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+   
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA. */
+
+#include "WCCGFloatType.h"
+
+@implementation WCCGFloatType 
+
+- (id) initWithObjcType: (NSString *)aType
+{
+  self = [super initWithObjcType: aType];
+  ASSIGN (objcType, aType);
+
+  if (sizeof (CGFloat) == 4)
+    {
+      javaType = @"float";
+      jniType = @"jfloat";
+      javaArgumentType = @"F";
+    }
+  else if (sizeof (CGFloat) == 8)
+    {
+      javaType = @"double";
+      jniType = @"jdouble";
+      javaArgumentType = @"D";
+    }
+  else 
+    {
+      NSLog (@"Warning: sizeof CGFloat is weird.");
+      javaType = @"double";
+      jniType = @"jdouble";
+      javaArgumentType = @"D";
+    }
+
+  return self;
+}
+@end
+
